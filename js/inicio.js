c9191a62d43348c46bc390a36861d6887859f6ea
archesspect <megazetap@gmail.com>
2021-10-12 22:13:12 -0300

    avance 2

--- js/inicio.js
+++ js/inicio.js
@@ -1,11 +1,88 @@
 "use strict";
-//Funcion para habilitar campos de solo lectura del formulario
-function editar() {
-    var rut = document.getElementById('rut').disabled=false;
-    var telefono = document.getElementById('telefono').disabled=false;
-    var correo = document.getElementById('email').disabled=false;
-    var acudientes = document.getElementById('acudientes').disabled=false;
-    var nacimiento = document.getElementById('nacimiento').disabled=false;
-    var direccion = document.getElementById('direccion').disabled=false;
-    var botonactualizar = document.getElementById('botonactualizar').disabled=false;
+//Región + Comuna
+var region = document.getElementById("region");
+region.addEventListener("change", actualizarComunas);
+var comuna = document.getElementById("comuna");
+function actualizarComunas() {
+    while (comuna.firstChild) {
+        comuna.removeChild(comuna.firstChild);
+    }
+    ;
+    switch (region.value) {
+        case "0": // vacio
+            var opcion_1 = document.createElement("option");
+            opcion_1.innerHTML = 'seleciona una comuna';
+            comuna.appendChild(opcion_1);
+            break;
+        case "1": // Valparaiso
+            var valparaiso = document.createElement("option");
+            valparaiso.innerHTML = 'Valparaíso';
+            comuna.appendChild(valparaiso);
+            var quilpue = document.createElement("option");
+            quilpue.innerHTML = 'Quilpue';
+            comuna.appendChild(quilpue);
+            var quillota = document.createElement("option");
+            quillota.innerHTML = 'Quillota';
+            comuna.appendChild(quillota);
+            var va = document.createElement("option");
+            va.innerHTML = 'Villa Alemana';
+            comuna.appendChild(va);
+            break;
+        case "2": { //Santiago
+            var renca = document.createElement("option");
+            renca.innerHTML = 'Renca';
+            comuna.appendChild(renca);
+            var pudahuel = document.createElement("option");
+            pudahuel.innerHTML = 'Pudahuel';
+            comuna.appendChild(pudahuel);
+            var quilicura = document.createElement("option");
+            quilicura.innerHTML = 'Quilicura';
+            comuna.appendChild(quilicura);
+            break;
+        }
+    }
+}
+function ValidarRut(valor) {
+    var tmp = valor.split('-');
+    var digito = tmp[1];
+    var rut = tmp[0];
+    if (digito == 'K')
+        digito = 'k';
+    var M = 0, S = 1;
+    for (; rut; rut = Math.floor(rut / 10))
+        S = (S + rut % 10 * (9 - M++ % 6)) % 11;
+    console.log(S ? S - 1 : 'k');
+    return S ? S - 1 : 'k';
+}
+// function enviar(e:any){
+//     e.preventDefault();
+//     console.log("hola mundo")
+// }
+var campos = [{
+        "id": "",
+        "rut": "",
+        "telefono": "",
+        "email": "",
+        "acudientes": "",
+        "direccion": ""
+    }
+];
+// form.addEventListener('submit', (event:any) => {event.preventDefault(); this.submitForm()});
+// elementFirst.innerHTML = 'email: ' + document.getElementById('loginForm')[0].value;
+// document.getElementById('loginForm').appendChild(elementFirst);
+// elementSecond.innerHTML = 'password: ' + document.getElementById('loginForm')[1].value;
+// document.getElementById('loginForm').appendChild(elementSecond);
+function desbloquear() {
+    document.getElementById("botondesbloqueo").hidden = false;
+    document.getElementById("rut").hidden = false;
+    document.getElementById("rutinicial").hidden = true;
+    document.getElementById("telefono").hidden = false;
+    document.getElementById("telefonoinicial").hidden = true;
+    document.getElementById("email").hidden = false;
+    document.getElementById("emailinicial").hidden = true;
+    document.getElementById("acudientes").hidden = false;
+    document.getElementById("acudientesinicial").hidden = true;
+    document.getElementById("direccion").hidden = false;
+    document.getElementById("direccioninicial").hidden = true;
+    document.getElementById("actualizar").hidden = false;
 }
--- ts/inicio.ts
+++ ts/inicio.ts
@@ -1,12 +1,84 @@
-//Funcion para habilitar campos de solo lectura del formulario y boton actualizar, al meter los datos
+//Región + Comuna
+let region:any=document.getElementById("region");
+  region.addEventListener("change", actualizarComunas);
+  let comuna:any=document.getElementById("comuna");
+  function actualizarComunas():void {
+      while (comuna.firstChild){comuna.removeChild(comuna.firstChild);};
+      switch (region.value) {
+          case "0":// vacio
+              let opcion_1:any=document.createElement("option");
+              opcion_1.innerHTML = 'seleciona una comuna';
+              comuna.appendChild(opcion_1);
+              break;
+          case "1": // Valparaiso
+              let valparaiso:any=document.createElement("option");   valparaiso.innerHTML = 'Valparaíso';  comuna.appendChild(valparaiso);
+              let quilpue:any=document.createElement("option"); quilpue.innerHTML = 'Quilpue'; comuna.appendChild(quilpue);
+              let quillota:any=document.createElement("option"); quillota.innerHTML = 'Quillota'; comuna.appendChild(quillota);              
+              let va:any=document.createElement("option"); va.innerHTML = 'Villa Alemana'; comuna.appendChild(va);
+              break;
+          case "2":{ //Santiago
+              let renca:any=document.createElement("option");   renca.innerHTML = 'Renca';  comuna.appendChild(renca);
+              let pudahuel:any=document.createElement("option"); pudahuel.innerHTML = 'Pudahuel'; comuna.appendChild(pudahuel);
+              let quilicura:any=document.createElement("option"); quilicura.innerHTML = 'Quilicura'; comuna.appendChild(quilicura);
+              break;
+          }
+      }
+  }
 
-function editar(){
-    let rut:any = document.getElementById('rut');
-    let telefono:any = document.getElementById('telefono');
-    let correo:any = document.getElementById('email');
-    let acudientes:any = document.getElementById('acudientes')
-    let nacimiento:any = document.getElementById('nacimiento');
-    let direccion:any = document.getElementById('direccion');  
-    let botonactualizar:any = document.getElementById('botonactualizar');
+function ValidarRut(valor:any){
+    var tmp=valor.split('-');
+    let digito=tmp[1];
+    let rut=tmp[0];
+    if(digito=='K') digito='k';
+    var M=0,S=1;
+    for(;rut;rut=Math.floor(rut/10))
+      S=(S+rut%10*(9-M++%6))%11;
+      console.log(S?S-1:'k');
+
+   return S?S-1:'k';
 }
+
+// function enviar(e:any){
+//     e.preventDefault();
+//     console.log("hola mundo")
+// }
+
+let campos=[{
+    "id": "",
+    "rut":"",
+    "telefono":"",
+    "email":"",
+    "acudientes":"",
+    "direccion":""
+    }
+]
+
+
+// form.addEventListener('submit', (event:any) => {event.preventDefault(); this.submitForm()});
+// elementFirst.innerHTML = 'email: ' + document.getElementById('loginForm')[0].value;
+// document.getElementById('loginForm').appendChild(elementFirst);
+
+// elementSecond.innerHTML = 'password: ' + document.getElementById('loginForm')[1].value;
+// document.getElementById('loginForm').appendChild(elementSecond);
+
+
+
+
+
+
+function desbloquear(){
+    (<HTMLInputElement> document.getElementById("botondesbloqueo")).hidden = false;
+    (<HTMLInputElement> document.getElementById("rut")).hidden = false;
+    (<HTMLInputElement> document.getElementById("rutinicial")).hidden = true;
+    (<HTMLInputElement> document.getElementById("telefono")).hidden = false;
+    (<HTMLInputElement> document.getElementById("telefonoinicial")).hidden = true;
+    (<HTMLInputElement> document.getElementById("email")).hidden = false;
+    (<HTMLInputElement> document.getElementById("emailinicial")).hidden = true;
+    (<HTMLInputElement> document.getElementById("acudientes")).hidden = false;
+    (<HTMLInputElement> document.getElementById("acudientesinicial")).hidden = true;
+    (<HTMLInputElement> document.getElementById("direccion")).hidden = false;
+    (<HTMLInputElement> document.getElementById("direccioninicial")).hidden = true;
+    (<HTMLInputElement> document.getElementById("actualizar")).hidden = false;
+}
+
 
